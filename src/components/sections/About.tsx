f7cfffeaf058ee00dd36d322857dab881f6b3a35
Sahil Jhajharia <sahiljhajharia@Sahils-MacBook-Air.local>
2025-07-11 23:43:25 +0530

    changes nothing

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -8,7 +8,7 @@ const About = () => {
       <div className="container mx-auto text-center px-4">
         <Animate delay={0.1}>
           <h2 className="text-5xl md:text-6xl font-bold mb-4 text-white tracking-tight">
-            Hi, I'm Sahil Jhajharia
+            Hi, I&apos;mm Sahil Jhajharia
           </h2>
           <div className="mx-auto w-48 h-0.5 mt-6 mb-8 bg-gradient-to-r from-transparent via-[#a174ff] to-transparent" />
         </Animate>
@@ -21,7 +21,7 @@ const About = () => {
 
         <Animate delay={0.3}>
           <p className="max-w-2xl mx-auto text-lg md:text-xl text-[#a2a2a5] tracking-normal leading-relaxed">
-            I'm a passionate software developer specializing in full-stack
+            I&apos;m a passionate software developer specializing in full-stack
             development, with expertise in building scalable web applications
             using modern frameworks like Next.js and Express.js. Based in India,
             I thrive on collaborating with teams to deliver innovative
